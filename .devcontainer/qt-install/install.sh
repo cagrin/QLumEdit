f54cb95a064d145341ce9eb57204f5189c111346
Krzysztof Strugiński <80916480+cagrin@users.noreply.github.com>
2025-07-06 21:07:01 +0000

    Add host, arch, spec params

--- .devcontainer/qt-install/install.sh
+++ .devcontainer/qt-install/install.sh
@@ -1,8 +1,14 @@
 QT_VERSION=6.8.3
-QT_ARCH="linux desktop"
-QT_SPEC=linux_gcc_64
+QT_HOST="linux desktop"
+QT_ARCH=linux_gcc_64
+QT_SPEC=gcc_64
+QT_OUTPUT_DIR=/opt/qt
+QT_PATH=$QT_OUTPUT_DIR/$QT_VERSION/$QT_SPEC/bin
 
 pip install aqtinstall
 
-aqt list-qt $QT_ARCH --arch $QT_VERSION
-aqt install-qt $QT_ARCH $QT_VERSION $QT_SPEC
+aqt list-qt $QT_HOST --arch $QT_VERSION
+aqt install-qt $QT_HOST $QT_VERSION $QT_ARCH --outputdir $QT_OUTPUT_DIR
+
+echo "export PATH=\"$QT_PATH:\$PATH\"" >> /etc/profile.d/qt-path.sh
+chmod +x /etc/profile.d/qt-path.sh
