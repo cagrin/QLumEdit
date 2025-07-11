38977333451e4dd91f4f3f84634e77a0c6138b33
Krzysztof Strugiński <cagrin@gmail.com>
2025-07-11 16:30:19 +0000

    Add aarch64 experimental support to devcontainer

--- .devcontainer/qt-install/install.sh
+++ .devcontainer/qt-install/install.sh
@@ -1,17 +1,24 @@
 QT_VERSION=6.8.3
-QT_HOST="linux desktop"
-QT_ARCH=linux_gcc_64
-QT_SPEC=gcc_64
+if [[ "$(uname -m)" == "aarch64" ]]; then
+  QT_HOST=linux_arm64
+  QT_ARCH=linux_gcc_arm64
+  QT_SPEC=gcc_arm64
+else
+  QT_HOST=linux
+  QT_ARCH=linux_gcc_64
+  QT_SPEC=gcc_64
+fi
 QT_OUTPUT_DIR=/opt/qt
 QT_PATH=$QT_OUTPUT_DIR/$QT_VERSION/$QT_SPEC/bin
 
-pip install aqtinstall
+apt-get update && \
+apt-get install -y pipx libgl1-mesa-dev libxkbcommon-x11-0 libxcb-cursor0 libxcb-keysyms1 libxcb-icccm4
+export PATH="$PATH:/root/.local/bin"
+pipx install aqtinstall
 
-aqt list-qt $QT_HOST --arch $QT_VERSION
-aqt install-qt $QT_HOST $QT_VERSION $QT_ARCH --outputdir $QT_OUTPUT_DIR
-
+aqt list-qt $QT_HOST desktop --arch $QT_VERSION
+aqt install-qt $QT_HOST desktop $QT_VERSION $QT_ARCH --outputdir $QT_OUTPUT_DIR
 echo "export PATH=\"$QT_PATH:\$PATH\"" >> /etc/profile.d/qt-path.sh
 chmod +x /etc/profile.d/qt-path.sh
 
-apt-get update
-apt-get install -y libgl1-mesa-dev libxkbcommon-x11-0 libxcb-cursor0 libxcb-keysyms1 libxcb-icccm4 
+apt-get clean && rm -rf /var/lib/apt/lists/*
